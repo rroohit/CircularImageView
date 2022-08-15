c05716fb070bc2ea1eba841fde8d54a6406e8157
crohi <crohitp9@gmail.com>
2022-08-15 20:01:48 +0530

    CircularImageView

---
+++ app/src/main/java/com/roh/sample/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.roh.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.roh.sample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+    }
+
+
+}
